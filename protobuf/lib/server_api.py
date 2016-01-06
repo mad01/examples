d345918bab293d0e0a86444f32ef2b4b8eb25081
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2016-01-06 18:02:16 +0100

    change error_data to data

--- protobuf/lib/server_api.py
+++ protobuf/lib/server_api.py
@@ -26,12 +26,12 @@ class CreateAcount(object):
     def on_post(self, req, resp):
         if not validate_content_type(req.content_type):
             errorMessage = 'content_type is: %s' % (req.content_type)
-            error_data = proto_error(
+            data = proto_error(
                 errorCode=proto.INCORRECT_CONTENT_TYPE,
                 message=errorMessage
                 )
             resp.status = falcon.HTTP_400
-            resp.body = error_data
+            resp.body = data
 
         try:
             req.stream.read()
