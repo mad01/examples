8e27d6d70d12a76537de2e859bbc59dd09140e20
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2016-05-02 14:03:23 +0200

    adding to copy ssh key

--- docker/python/jenkins.sh
+++ docker/python/jenkins.sh
@@ -3,5 +3,8 @@ set -o errexit
 set -o xtrace
 set -o pipefail
 
+cp $HOME/.ssh/id_rsa id_rsa_docker
+cp $HOME/.ssh/id_rsa.pub id_rsa_docker.pub
+
 docker-compose build --no-cache
 docker-compose run tester
