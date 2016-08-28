cb0960f590d08fa72faab5e0657da6bb70cec918
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2016-08-28 18:28:50 +0200

    change structure some to not make to long lines and only import used funcs

--- prometheus/python/prom_exporter/handler.py
+++ prometheus/python/prom_exporter/handler.py
@@ -1,7 +1,8 @@
 import falcon
 
-import prometheus_client
 from wsgiref import simple_server
+from prometheus_client.exposition import CONTENT_TYPE_LATEST
+from prometheus_client.exposition import generate_latest
 
 from prom_exporter.prom import Collector
 
@@ -13,15 +14,23 @@ class metricHandler:
         self._exclude = exclude
 
     def on_get(self, req, resp):
-        resp.set_header('Content-Type', prometheus_client.exposition.CONTENT_TYPE_LATEST)
-        registry = Collector(self._url, self._service, exclude=self._exclude)
-        collected_metric = prometheus_client.exposition.generate_latest(registry)
+        resp.set_header('Content-Type', CONTENT_TYPE_LATEST)
+        registry = Collector(
+            self._url,
+            self._service,
+            exclude=self._exclude
+            )
+        collected_metric = generate_latest(registry)
         resp.body = collected_metric
 
 
 def falcon_app(url, service, port=9999, addr='0.0.0.0', exclude=list):
+    print('starting server http://127.0.0.1:{}/metrics'.format(port))
     api = falcon.API()
-    api.add_route('/metrics', metricHandler(url=url, service=service, exclude=exclude))
+    api.add_route(
+        '/metrics',
+        metricHandler(url=url, service=service, exclude=exclude)
+    )
 
     httpd = simple_server.make_server(addr, port, api)
     httpd.serve_forever()
